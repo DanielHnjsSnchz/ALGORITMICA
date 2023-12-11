50bec9915e87198f389d2b2b7765fb49ee125fff
DanielHnjsSnchz <101149931+DanielHnjsSnchz@users.noreply.github.com>
2023-12-11 21:09:21 +0100

    Add files via upload

--- P5/funcionesBacktracking.cpp
+++ P5/funcionesBacktracking.cpp
@@ -1,12 +1,11 @@
-#include <vector>
 #include <iostream>
-
+#include <vector>
+#include <cstdlib>
 #include "ClaseTiempo.cpp"
 
 using namespace std;
 
 bool esSeguro(vector<int>& solucion, int fila, int columna) {
-    // Verifica si es seguro colocar una reina en esta posición
     for (int i = 0; i < fila; ++i) {
         if (solucion[i] == columna || abs(solucion[i] - columna) == abs(i - fila)) {
             return false;
@@ -15,48 +14,93 @@ bool esSeguro(vector<int>& solucion, int fila, int columna) {
     return true;
 }
 
-void resolverNReinasBacktracking(int fila, int n, vector<int>& solucion, vector<vector<int>>& soluciones) {
+void resolverNReinasBacktracking(int fila, int n, vector<int>& solucion, vector<vector<int>>& soluciones, int& intentos) {
     if (fila == n) {
-        // Se ha encontrado una solución completa
         soluciones.push_back(solucion);
+        ++intentos;
         return;
     }
 
     for (int columna = 0; columna < n; ++columna) {
         if (esSeguro(solucion, fila, columna)) {
             solucion[fila] = columna;
-            resolverNReinasBacktracking(fila + 1, n, solucion, soluciones);
+            resolverNReinasBacktracking(fila + 1, n, solucion, soluciones, intentos);
         }
     }
 }
 
-void nReinasBacktracking(int n, vector<vector<int>>& soluciones) {
+void nReinasBacktracking(int n) {
     vector<int> solucion(n, -1);
-    resolverNReinasBacktracking(0, n, solucion, soluciones);
-}
+    vector<vector<int>> soluciones;
+    Clock reloj;
+    reloj.start();
 
-void escribirSoluciones(vector<vector<int>>& soluciones) {
+    int intentos = 0;
+    resolverNReinasBacktracking(0, n, solucion, soluciones, intentos);
+
+    reloj.stop();
+
+    // Mostrar todas las soluciones encontradas
+    cout << "Todas las soluciones del problema para n=" << n << ":\n";
     for (const auto& solucion : soluciones) {
         for (int col : solucion) {
             cout << col << " ";
         }
         cout << "\n";
     }
-}
 
-void nReinasBacktracking(int n, vector<int>& solucion) {
-    Clock reloj;
-    reloj.start();
-
-    resolverNReinasBacktracking(0, n, solucion, {});
-
-    reloj.stop();
+    // Mostrar el número de intentos y el tiempo transcurrido
+    cout << "Número total de soluciones encontradas: " << soluciones.size() << "\n";
+    cout << "Número de intentos: " << intentos << "\n";
     cout << "Tiempo transcurrido: " << reloj.elapsed() << " microsegundos\n";
 }
 
-void escribirSolucion(vector<int>& solucion) {
-    for (size_t i = 0; i < solucion.size(); ++i) {
-        cout << "\t" << solucion[i] <<endl;
+void resolverNReinasBacktrackingUnica(int fila, int n, vector<int>& solucion, int& intentos) {
+    if (fila == n) {
+        ++intentos;
+        return;
+    }
+
+    for (int columna = 0; columna < n; ++columna) {
+        if (esSeguro(solucion, fila, columna)) {
+            solucion[fila] = columna;
+            resolverNReinasBacktrackingUnica(fila + 1, n, solucion, intentos);
+            if (intentos > 0) {
+                return;
+            }
+        }
+    }
+}
+
+void nReinasBacktrackingUnica(int n) {
+    vector<int> solucion(n, -1);
+    Clock reloj;
+    reloj.start();
+
+    int intentos = 0;
+    resolverNReinasBacktrackingUnica(0, n, solucion, intentos);
+
+    reloj.stop();
+
+    // Mostrar la primera solución encontrada
+    cout << "La primera solución encontrada para n=" << n << ":\n";
+    for (int col : solucion) {
+        cout << col << " ";
     }
     cout << "\n";
+
+    // Mostrar el número de intentos y el tiempo transcurrido
+    cout << "Número de intentos: " << intentos << "\n";
+    cout << "Tiempo transcurrido: " << reloj.elapsed() << " microsegundos\n";
+}
+
+int main() {
+    int n;
+    cout << "Introduce el valor de n: ";
+    cin >> n;
+
+    nReinasBacktracking(n);
+    nReinasBacktrackingUnica(n);
+
+    return 0;
 }
